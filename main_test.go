713364b9174657c709116344ca2be820edf0e79c
bhimantoro <bhimantoros@gmail.com>
2021-08-12 21:45:57 +0700

    connect database unit test

--- main_test.go
+++ main_test.go
@@ -1,14 +1,20 @@
 package main_test
 
 import (
+	"log"
 	"os"
 	"restgo/api/controllers"
 	"testing"
+
+	"github.com/joho/godotenv"
 )
 
 var app controllers.App
 
 func TestMain(m *testing.M) {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	app.Initialize(
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
