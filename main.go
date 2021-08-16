c59dea5245a25de4b075f2c5724e3f80a7b93f69
bhimantoro <bhimantoros@gmail.com>
2021-08-17 00:08:02 +0700

    change env

--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"log"
 	"restgo/api/controllers"
 
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// if err := godotenv.Load(); err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 	app := controllers.App{}
 	app.Initialize()
 	app.RunServer()
