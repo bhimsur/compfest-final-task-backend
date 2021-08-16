b535f2310916dae855605ac71e911b264f0ae45c
danielcm585 <74773920+danielcm585@users.noreply.github.com>
2021-08-16 16:56:18 +0700

    Add files via upload

--- api/controllers/donationController.go
+++ api/controllers/donationController.go
@@ -21,3 +21,29 @@ func (a *App) GetDonationHistoryFromUser(w http.ResponseWriter, r *http.Request)
 		return
 	}
 }
+
+func (a *App) DonateNow(w http.ResponseWriter, r *http.Request) {
+	var resp = map[string]interface{}{"status": true, "message": "success"}
+
+	user := r.Context().Value("UserID").(float64)
+	userID := int(user)
+	amount := r.Context().Value("Amount").(float64)
+	donationProgram := r.Context().Value("DonationProgram")	
+
+	newDonation := &models.Donation{
+		Amount: amount,
+		UserID: userID,
+		DonationProgram: donationProgram,
+		// TODO: Figure out how to get donationProgramID
+		DonationProgramID: donationProgram.ID,
+	}
+
+	donationCreated, err := newDonation.SaveDonation(a.DB)
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+	resp["data"] = donationCreated
+	responses.JSON(w, http.StatusCreated, resp)
+	return
+}
--- api/controllers/topUpController.go
+++ api/controllers/topUpController.go
@@ -10,37 +10,46 @@ import (
 
 func (a *App) CreateTopUp(w http.ResponseWriter, r *http.Request) {
 	var resp = map[string]interface{}{"status": true, "message": "success"}
-	user := r.Context().Value("UserID").(float64)
-	userId := int(user)
+	userIdTmp := r.Context().Value("UserID").(float64)
+	userId := int(userIdTmp)
+	amount := r.Context().Value("Amount").(float64)
+	user, err := models.GetUserById(userId)
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return 
+	}
 
-	topUp := &models.TopUp{}
+	topUp := &models.TopUp{
+		Amount: amount,
+		User: user,
+		UserID, userId,
+	}
+
+	// NOTES: Bagian sini ngapain sih?
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		responses.ERROR(w, http.StatusBadRequest, err)
 		return
 	}
-	err = json.Unmarshal(body, &topUp)
-	if err != nil {
+	if err = json.Unmarshal(body, &topUp), err != nil {
 		responses.ERROR(w, http.StatusBadRequest, err)
 		return
 	}
-
 	if err = topUp.Validate(); err != nil {
 		responses.ERROR(w, http.StatusBadRequest, err)
 		return
 	}
 
-	topUp.UserID = uint(userId)
 	topUpCreated, err := topUp.CreateTopUp(a.DB)
-	walletUpdate := models.Wallet{}
-	walletUpdate.Amount = topUp.Amount + walletUpdate.Amount
-	_, _ = walletUpdate.UpdateWalletFromTopUpByUserId(userId, walletUpdate.Amount, a.DB)
+	// NOTES: Udah dipindah ke models.topUp
+	// walletUpdate := models.Wallet{}
+	// walletUpdate.Amount = topUp.Amount + walletUpdate.Amount
+	// _, _ = walletUpdate.UpdateWalletFromTopUpByUserId(userId, walletUpdate.Amount, a.DB)
 	if err != nil {
 		responses.ERROR(w, http.StatusBadRequest, err)
 		return
-	} else {
-		resp["data"] = topUpCreated
-		responses.JSON(w, http.StatusCreated, resp)
-		return
 	}
+	resp["data"] = topUpCreated
+	responses.JSON(w, http.StatusCreated, resp)
+	return
 }
