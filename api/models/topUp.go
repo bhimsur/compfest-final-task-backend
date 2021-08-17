e561a3bd6c12024caa1a58a47ac6921e396d7705
bhimantoro <bhimantoros@gmail.com>
2021-08-17 14:51:03 +0700

    add endpoints to topup history

--- api/models/topUp.go
+++ api/models/topUp.go
@@ -2,14 +2,21 @@ package models
 
 import (
 	"errors"
+	"time"
 
 	"github.com/jinzhu/gorm"
 )
 
 type TopUp struct {
+	gorm.Model
 	Amount float64 `json:"amount"`
 	User   User    `gorm:"foreignKey:UserID" json:"user"`
 	UserID uint    `json:"user_id"`
+}
+
+type TopUpAPI struct {
+	Amount    float64   `json:"amount"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 func (t *TopUp) Validate() error {
@@ -24,4 +31,12 @@ func (t *TopUp) CreateTopUp(db *gorm.DB) (*TopUp, error) {
 		return &TopUp{}, err
 	}
 	return t, nil
+}
+
+func GetTopUpHistoryByUserId(user_id int, db *gorm.DB) (*[]TopUpAPI, error) {
+	topUps := []TopUpAPI{}
+	if err := db.Debug().Table("top_ups").Where("user_id = ?", user_id).Find(&topUps).Error; err != nil {
+		return &[]TopUpAPI{}, err
+	}
+	return &topUps, nil
 }
