d418d5a103ff32b6f583046843883872e65af1f8
bhimantoro <bhimantoros@gmail.com>
2021-08-18 22:25:27 +0700

    options

--- api/middlewares/middlewares.go
+++ api/middlewares/middlewares.go
@@ -12,6 +12,13 @@ import (
 
 func SetResponsesMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Authorization, X-Requested-With, Content-Type")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Content-Type", "application/json")
+		}
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080, http://localhost:3000, https://pentapeduli.hexalogi.cyou/")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Authorization, X-Requested-With, Content-Type")
